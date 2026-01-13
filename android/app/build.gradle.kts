cf62dc2ad00e523514106b7a5692d69c3f63539f
Shafiuzzaman <uzzamanshafi511@gmail.com>
2026-01-13 20:56:50 +0600

    Fix namespace and application ID in build.gradle.kts to remove redundancy

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.shafisma.expense_tracker.expense_tracker"
+    namespace = "com.shafisma.expense_tracker"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.shafisma.expense_tracker.expense_tracker"
+        applicationId = "com.shafisma.expense_tracker"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
